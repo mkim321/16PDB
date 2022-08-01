8ad2a4f6c9dbdf6fa84aadd201b68d5666c7592c
Misun <eeesolar27@gmail.com>
2022-07-31 17:25:47 -0700

    4x4 Grid View
    Build a TextInput for search bar
    Build TouchableOpacity buttons for button (4x4 grid)
    Build a button for Test
    
    Link is empty but will figure it out
    
    Had a lot of error from MUI
    We won't use for in the home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,50 @@
-import { StyleSheet, View } from "react-native";
+import * as React from 'react';
 
+import { StyleSheet, View, TouchableOpacity, Image, Button, TextInput } from "react-native";
 import { ThemeProvider, Text } from "react-native-elements";
 
 import { typeTheme } from "../themes/typeTheme";
 
 export default function HomeScreen({ navigation }) {
+    const [text, onChangeText] = React.useState("Search");
     return(
         <ThemeProvider theme={typeTheme}>
             <View style={styles.container}>
-                <Text>Hello</Text>
+            <TextInput
+                style={styles.input}
+                onChangeText={onChangeText}
+                value={text}
+            />
+                <Text h2>Personality Group Types</Text>
+                <View style={styles.btngrid}>
+                    <View style={styles.row}>
+                        <TouchableOpacity style ={styles.imgbtn}>
+                            <Image
+                            style={styles.imagebtn}
+                            source={require('../assets/img/home-analyst.png')}/>
+                        </TouchableOpacity>
+                        <TouchableOpacity style ={styles.imgbtn}>
+                            <Image
+                            style={styles.imagebtn}
+                            source={require('../assets/img/home-diplomat.png')}/>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.row}>
+                        <TouchableOpacity style ={styles.imgbtn}>
+                            <Image
+                            style={styles.imagebtn}
+                            source={require('../assets/img/home-sentinel.png')}/>
+                        </TouchableOpacity>
+                        <TouchableOpacity style ={styles.imgbtn}>
+                            <Image
+                            style={styles.imagebtn}
+                            source={require('../assets/img/home-explorer.png')}/>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            <Text h2>Take The Test</Text>
+            <Button style={styles.btn} title='Official 16 Personality Test' color='#D6E9E1'></Button>
+
             </View>
         </ThemeProvider>
     );
@@ -17,5 +53,42 @@ export default function HomeScreen({ navigation }) {
 const styles = StyleSheet.create({
     container: {
       flex: 1,
+      paddingHorizontal: 25,
+      backgroundColor: '#fff'
     },
+    input:{
+        fontFamily: 'Montserrat_500Medium',
+        backgroundColor: '#F3F3F3',
+        fontSize: 16,
+        paddingVertical:10,
+        paddingHorizontal:20,
+        borderRadius:30,
+        marginTop:24,
+        marginBottom: 36
+    },
+    btngrid:{
+        // position:'absolute',
+        flex: 1,
+        flexWrap: 'wrap',
+    },
+    row:{
+        flex:1,
+        flexDirection:'row',
+        justifyContent:'space-between',
+    },
+    imgbtn:{        
+        width: 160,
+        height: 210,
+        marginBottom:20,
+    },
+    btn: {
+        color: '#372F3A',
+        fontFamily: 'Montserrat_500Medium',
+        marginBottom: 20,
+    },
+    imagebtn: {
+        width:160,
+        height: 210
+    }
+
   });
--- themes/typeTheme.js
+++ themes/typeTheme.js
@@ -6,10 +6,21 @@ const themePalette = {
     greenLighter: '#D6E9E1',
     gray: '#F3F3F3',
     blueDarker:'#4991AA',
+    textGray: '#372f3a'
 }
 
 export const typeTheme = {
     Text: {
+        h2Style:{
+            fontFamily: 'Montserrat_600SemiBold',
+            fontWeight: 'normal',
+            fontSize: 22,
+            textAlign: 'center',
+            color: themePalette.textGray,
+            textTransform: 'uppercase',
+            marginBottom: 20,
+        
+        },
         style:{
             color: themePalette.green,
             fontWeight: 'normal',
